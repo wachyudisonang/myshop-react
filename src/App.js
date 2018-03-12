36926f115ed430fe82ab356a076cf2ac50f90f3b
Wachyudi Sonang <wachyudi.sonang@gmail.com>
2018-03-12 18:55:20 +0700

    implement react.search

--- src/App.js
+++ src/App.js
@@ -1,21 +1,197 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+    Container,
+    Row,
+    Col,
+    Jumbotron,
+    Button
+} from 'reactstrap';
+import axios from 'axios';
+import NavbarSection from './components/navbar';
+import AddProduct from './components/AddProduct';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            shown: true,
+            products: [],
+            selectedOption: '',
+            selectedOption2: ''
+        }
+    }
+
+    toggle() {
+		this.setState({
+			shown: !this.state.shown
+		});
+	}
+    
+    componentDidMount() {
+        /* fetch API in action */
+        axios.get('http://127.0.0.1:8000/api/product_categories')
+            .then(function (response) {
+                return response;
+            })
+            .then(products => {
+                //Fetched product is stored in the state
+                this.setState({ products: products.data });
+            });
+    }
+
+    renderDataSource(product, index) {
+        return (
+            <li key={product.id} onClick={
+                () =>this.handleClick(product)} >
+                { product.Name } 
+            </li>
+        )
+    }
+
+    renderData(product, index) {
+        return (
+            { value: product.ID, label: product.Name }
+        )
+    }
+    renderData2(product, index) {
+        return (
+            { value: product.ID, label: product.Name }
+        )
+    }
+
+    plus = (selectedOption2) => {
+        this.setState({ selectedOption2 });
+        console.log(`Selected: ${selectedOption2.label}`);
+    }
+
+    handleChange = (selectedOption) => {
+        // console.log(`selectedOption: ${selectedOption.label}, plus: ${plus}`)
+        this.toggle();
+        this.setState({ selectedOption });
+        console.log(`Selected: ${selectedOption.label}`);
+    }
+    
+    handleChangeProduct = (selectedOption2) => {
+        // this.toggle();
+        this.setState({ selectedOption2 });
+        console.log(`Selected: ${selectedOption2.label}`);
+    }
+
+    handleAddProduct(product) {
+        // product.price = Number(product.price);
+        /*Fetch API for post request */
+        // fetch( 'http://127.0.0.1:8000/api/products', {
+        //     method:'post',
+        //     /* headers are important*/
+        //     mode: 'no-cors',
+        //     headers: {
+        //         // 'Access-Control-Allow-Origin': '*',
+        //       'Accept': 'application/json',
+        //       'Content-Type': 'application/json',
+        //     //   'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS'
+        //     },
+            
+        //     body: JSON.stringify(product)
+        // })
+        axios({
+            method: 'POST',
+            url: 'http://127.0.0.1:8000/api/products',
+            data: JSON.stringify(product),
+            config: { headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }}
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then( data => {
+           
+            this.setState((prevState)=> ({
+                products: prevState.products.concat(data),
+                currentProduct : data
+            }))
+        })
+    }
+
+    render() {
+        var shown = {
+			display: this.state.shown ? "block" : "none"
+		};
+		
+		var hidden = {
+			display: this.state.shown ? "none" : "block"
+        }
+        
+        const divStyle = {
+            justifyContent: "flex-start",
+            width: '35%',
+            background: '#f0f0f0',
+            padding: '20px 20px 20px 20px',
+            margin: '30px 10px 10px 30px'
+        }
+
+        const { selectedOption } = this.state;
+        const { selectedOption2 } = this.state;
+        const value = selectedOption && selectedOption.value;
+        const value2 = selectedOption2 && selectedOption2.value2;
+
+        return (
+            <div>
+                <NavbarSection />
+                <Jumbotron>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <h1>Welcome to React</h1>
+                                <p>
+                                    <Button
+                                        tag="a"
+                                        color="success"
+                                        size="large"
+                                        href="http://reactstrap.github.io"
+                                        target="_blank"
+                                    >
+                                        View Reactstrap Docs
+                                    </Button>
+                                </p>
+                                <Select.Creatable
+                                    name="form-field-name"
+                                    value={value}
+                                    onChange={this.handleChange}
+                                    options={ this.state.products.map(this.renderData) }
+                                />
+                            </Col>
+                        </Row>
+                    </Container>
+                    <Container style={ shown }>
+                        <div style={divStyle}>
+                            <h3> All products </h3>
+                            <ul>
+                                { this.state.products.map(this.renderDataSource) }
+                                {/* { this.renderProducts() } */}
+                            </ul> 
+                        </div>
+                    </Container>
+                    <Container style={ hidden }>
+                        <h3>Add New Product</h3>
+                        <AddProduct onAdd={this.handleAddProduct} />
+                        {/* Category: <Select.Creatable
+                                    name="form-field-name-product"
+                                    value={value2}
+                                    onChange={this.plus}
+                                    options={ this.state.products.map(this.renderData2) }
+                                />
+                        Name: {this.state.selectedOption.label} */}
+                    </Container>
+                </Jumbotron>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/AddProduct.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+
+class AddProduct extends Component {
+
+  constructor(props) {
+    super(props);
+       /* Initialize the state. */
+       this.state = {
+          newProduct: {
+              Name: '',
+              Variant: '',
+              PackSize: 0,
+              Key: '',
+              CategoryID: 0,
+              UnitID: 0
+          }
+        }
+    
+    //Boilerplate code for binding methods with `this`
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+  }
+  
+  /* This method dynamically accepts inputs and stores it in the state */
+  handleInput(key, e) {
+    
+    /*Duplicating and updating the state */
+    var state = Object.assign({}, this.state.newProduct); 
+    state[key] = e.target.value;
+    state["Key"] = (state.Name+'_'+state.Variant+'_'+state.PackSize).toUpperCase();
+    // console.log(state);
+    this.setState({newProduct: state });
+  }
+ /* This method is invoked when submit button is pressed */
+  handleSubmit(e) {
+    //preventDefault prevents page reload   
+    e.preventDefault();
+    /*A call back to the onAdd props. The control is handed over
+     *to the parent component. The current state is passed 
+     *as a param
+     */
+    this.props.onAdd(this.state.newProduct);
+  }
+
+  render() {
+    const divStyle = {
+      position: 'absolute',
+      left: '35%',
+      top: '60%',
+      flexDirection: 'space-between',
+      
+      marginLeft: '30px'
+    }
+    
+    const inputStyle = {
+      margin: '0px 10px 0px 10px',
+      textTransform: 'capitalize'
+    }
+    return(
+      <div> 
+       
+        <div style={divStyle}> 
+         <h2> Add new Test product </h2>
+        {/*when Submit button is pressed, the control is passed to 
+         *handleSubmit method 
+         */}
+        <form onSubmit={this.handleSubmit}>
+
+          <label> 
+            Category: 
+            <input style={inputStyle}  type="number" onChange={(e)=>this.handleInput('CategoryID',e)} />
+          </label>
+          <label> 
+            Name: 
+            {/*On every keystroke, the handeInput method is invoked */}
+            <input style={inputStyle} type="text" onChange={(e)=>this.handleInput('Name',e)} />
+          </label>
+          <label> 
+            Variant: 
+            <input style={inputStyle}  type="text" onChange={(e)=>this.handleInput('Variant',e)} />
+          </label>
+          <label> 
+            Pack Size: 
+            <input style={inputStyle}  type="number" onChange={(e)=>this.handleInput('PackSize',e)} />
+          </label>
+          <label>
+            Unit:
+            <input style={inputStyle}  type="number" onChange={(e)=>this.handleInput('UnitID', e)}/>
+          </label>
+
+          <input style={inputStyle}  type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>)
+  }
+}
+
+export default AddProduct;
+  
